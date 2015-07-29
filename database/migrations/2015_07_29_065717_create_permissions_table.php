debe1ada5224d31c8cf59a84a7c22f548c874347
Grzegorz Pawlik <pawlik.gregor@gmail.com>
2015-07-29 07:03:08 +0000

    added permission migration

---
+++ database/migrations/2015_07_29_065717_create_permissions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string("permission_name");
+            $table->string("");
+
+            $table -> integer('role_id')->unsigned();
+			      $table -> foreign('role_id')->references('id')->on('roles');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('permissions');
+    }
+}
